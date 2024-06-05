105f521e332a18a886ad9b8ab6e672e3155c5337
Fabien Costa <fabiencosta94800@gmail.com>
2024-06-05 18:27:45 +0200

    first commit

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -15,7 +15,6 @@ export const api = {
     return json.json();
   },
 };
-
 export const DataProvider = ({ children }) => {
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
@@ -30,7 +29,7 @@ export const DataProvider = ({ children }) => {
     if (data) return;
     getData();
   });
-  
+
   return (
     <DataContext.Provider
       // eslint-disable-next-line react/jsx-no-constructed-context-values
@@ -46,7 +45,7 @@ export const DataProvider = ({ children }) => {
 
 DataProvider.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
 export const useData = () => useContext(DataContext);
 
