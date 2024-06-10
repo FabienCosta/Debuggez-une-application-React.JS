045e6781c62ab70129f58952d38ccc9f3826a9db
Fabien Costa <fabiencosta94800@gmail.com>
2024-06-10 12:06:22 +0200

    slider ok

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,10 +8,12 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    new Date(evtA.date) < new Date(evtB.date) ? -1 : 0
   );
+
   const nextCard = () => {
     setTimeout(
+      // j/ai ajouter -1 a la longueur du tableau pour que l'index ne depasse pas la longueur du tableau
       () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
       5000
     );
@@ -45,7 +47,8 @@ const Slider = () => {
                   key={`${event.id}`}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
+                  // l'etat de checked n'est pas gerer par idx mais par index, les radio se mettent a jour maintenant
+                  checked={index === radioIdx}
                 />
               ))}
             </div>
